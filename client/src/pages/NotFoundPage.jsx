c21d7b9d39a671729bcd8b9307eff418dcb1ad8d
urssanjaysingh <sanjaysingh26112000@gmail.com>
2025-04-13 14:12:52 +0530

    Not Found Page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFoundPage = () => {
     return (
-        <div className="flex items-center justify-center h-screen px-4">
+        <div className="px-4 py-16">
             <div className="text-center">
                 <h1 className="text-9xl font-extrabold text-gray-300">404</h1>
                 <p className="text-2xl md:text-3xl text-gray-700 mt-4 font-semibold">
