57931c97faf8aa03bb3d5b628ddbab46423fda09
urssanjaysingh <sanjaysingh26112000@gmail.com>
2025-04-14 09:36:14 +0530

    Delete Product

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -122,7 +122,7 @@ const ProductList = () => {
                                             onClick={() =>
                                                 confirmDelete(product._id)
                                             }
-                                            className="text-sm px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
+                                            className="text-sm cursor-pointer px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
                                         >
                                             Delete
                                         </button>
